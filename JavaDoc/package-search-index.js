08cb979776d929cbb4ba21d4f6553a5be4d1a723
Abi <mantaspa30@gmail.com>
2023-06-09 23:48:24 +0300

    Delete

--- JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ecosystem.Animal"},{"l":"ecosystem.Factory"},{"l":"ecosystem.Main"},{"l":"ecosystem.Plant"}];updateSearchResults();
